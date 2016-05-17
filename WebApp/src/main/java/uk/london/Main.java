ff0cbe438d0822ecd55134b4473fdc12ac0eadd8
Sabrina <sab.wons@hotmail.fr>
2016-05-17 17:10:22 +0100

    Modif Main

--- WebApp/src/main/java/uk/london/Main.java
+++ WebApp/src/main/java/uk/london/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println(sayHello());
-
+		System.out.println("do other thing");
 	}
 	
 	public static String sayHello(){
